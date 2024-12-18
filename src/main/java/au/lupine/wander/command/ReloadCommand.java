6cecd842eeee6e11748a5344205eac45f325300d
jwkerr <49851457+jwkerr@users.noreply.github.com>
2024-12-18 18:26:31 +1100

    Improve command more

---
+++ src/main/java/au/lupine/wander/command/ReloadCommand.java
@@ -0,0 +1,22 @@
+package au.lupine.wander.command;
+
+import au.lupine.wander.manager.TradeManager;
+import com.google.gson.JsonParseException;
+import dev.jorel.commandapi.executors.CommandArguments;
+import dev.jorel.commandapi.executors.CommandExecutor;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.command.CommandSender;
+
+public class ReloadCommand implements CommandExecutor {
+
+    @Override
+    public void run(CommandSender sender, CommandArguments args) {
+        try {
+            TradeManager.getInstance().reload();
+            sender.sendMessage(Component.text("Successfully reloaded Wander", NamedTextColor.GREEN));
+        } catch (JsonParseException e) {
+            sender.sendMessage(Component.text("Failed to reload Wander, there was an error parsing the JSON", NamedTextColor.RED));
+        }
+    }
+}
--- src/main/java/au/lupine/wander/manager/CommandManager.java
+++ src/main/java/au/lupine/wander/manager/CommandManager.java
@@ -1,9 +1,7 @@
 package au.lupine.wander.manager;
 
-import com.google.gson.JsonParseException;
+import au.lupine.wander.command.ReloadCommand;
 import dev.jorel.commandapi.CommandAPICommand;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
 
 public class CommandManager {
 
@@ -19,14 +17,7 @@ public class CommandManager {
     public void initialise() {
         CommandAPICommand reload = new CommandAPICommand("reload")
                 .withPermission("wander.command.wander.reload")
-                .executes((sender, args) -> {
-                    try {
-                        TradeManager.getInstance().reload();
-                        sender.sendMessage(Component.text("Successfully reloaded Wander", NamedTextColor.GREEN));
-                    } catch (JsonParseException e) {
-                        sender.sendMessage(Component.text("Failed to reload wander, there was an error parsing the JSON"));
-                    }
-                });
+                .executes(new ReloadCommand());
 
         CommandAPICommand command = new CommandAPICommand("wander")
                 .withSubcommand(reload);
